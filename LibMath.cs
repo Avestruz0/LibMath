86d82811091f7a285925921a6ffb198d2588fc9c
Lucas Campos <lucas10989316@edu.pa.senac.br>
2024-06-17 21:01:46 -0300

    Nova classe adicionada

---
+++ LibMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMath
+{
+    internal class LibMath
+    {
+        public float SomaEntre2valores(float v1, float v2) {
+
+        return v1 + v2; 
+    
+          }
+    }
+}
