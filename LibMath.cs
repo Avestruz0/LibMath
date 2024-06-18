567d15c64902fd21d15f61f2617a83cf8729d66c
Kevin Nunes <kevin51606916@edu.pa.senac.br>
2024-06-17 21:43:04 -0300

    Add função area do quadrado

--- LibMath.cs
+++ LibMath.cs
@@ -8,10 +8,13 @@ namespace LibMath
 {
     internal class LibMath
     {
-        public float SomaEntre2valores(float v1, float v2) {
-
-        return v1 + v2; 
-    
-          }
+        public float SomaEntre2valores(float v1, float v2)
+        {
+            return v1 + v2;
+        }
+        public float AreaDoQuadrado(float v1, float v2)
+        {
+            return v1 * v2;
+        }
     }
 }
