d56d8496b05488a2b28472a9127827966de551c9
Neila <neila48377036@edu.pa.senac.br>
2024-06-17 21:42:15 -0300

    Add função de area Retangulo

--- LibMath.cs
+++ LibMath.cs
@@ -8,10 +8,21 @@ namespace LibMath
 {
     internal class LibMath
     {
-        public float SomaEntre2valores(float v1, float v2) {
+        //public float SomaEntre2valores(float v1, float v2) {
 
-        return v1 + v2; 
+        //return v1 + v2; 
     
-          }
-    }
+          //}
+
+		public double areaRetangulo(float b, float h)
+		{
+			Console.WriteLine("Valor da base: ");
+			float b = Console.ReadLine();
+			Console.WriteLine("Valor da Altura h: ");
+			h = Console	.ReadLine();
+			return b * h;
+			
+		}
+
+	}
 }
